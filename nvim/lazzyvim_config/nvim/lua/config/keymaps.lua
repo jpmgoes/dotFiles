82e127605263a6a4c3d4adbe0dfcfd97b716132a
jpmourag <jp.mouragoes@gmail.com>
2024-02-11 20:17:13 +0000

    adding terminal keymap

--- nvim/lazzyvim_config/nvim/lua/config/keymaps.lua
+++ nvim/lazzyvim_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>wht", ":split | terminal<cr>a", { desc = "[O]pen [H]orizontal [T]erminal" })
+vim.keymap.set("n", "<leader>wvt", ":vsplit | terminal<cr>a", { desc = "[O]pen [V]ertical [T]erminal" })
